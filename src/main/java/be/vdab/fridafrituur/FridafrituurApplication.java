d4cd97a6e2643ebcabfb0a3f94e217f006b6dd6d
Nape Kona <nape.kona.zvn@hotmail.com>
2020-10-08 12:37:14 +0200

    FridafrituurApplication

---
+++ src/main/java/be/vdab/fridafrituur/FridafrituurApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.fridafrituur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridafrituurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridafrituurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/fridafrituur/FridafrituurApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.fridafrituur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridafrituurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
